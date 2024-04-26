64aadd32176f96fccadfc83e3c8e32d3829f4c60
Yashodhan Devdhar <139955827+YashodhanDevdhar@users.noreply.github.com>
2024-04-26 07:27:20 +0530

    6_1_navigation

--- main.dart
+++ main.dart
@@ -1,14 +1,69 @@
-
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('Insert Image Demo')),
-      body: Center(
-        child: Image.asset('assets/images/famtitlab.jpg'),
-      ),
+
+//Example for explaining Flutter Navigation and Routing
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Navigation',
+    theme: ThemeData(
+// This is the theme of your application.
+      primarySwatch: Colors.green,
     ),
-  );
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+                textStyle: TextStyle(fontSize: 20),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.cyan,
+
+          ),
+
+          child: Text('Click Here'),
+          
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Screen"),
+      ),
+      body: Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+                textStyle: TextStyle(fontSize: 20),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.cyan,
+
+          ),
+          
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back'),
+        ),
+      ),
+    );
+  }
 }
